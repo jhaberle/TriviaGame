aeacb91805322d1e37295e72b529e23356c5ac76
Jake Haberle <jake.haberle@gmail.com>
2020-03-01 20:33:06 -0700

    basic updates on html.  Two questions entered as vars in js

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var question1 = ["Kramer has a devious friend that lives down the hallway that loathes Jerry.  What is that friend's name?"];
+
+var answers1 = ["Lomez", "Newman", "Jay Remenschnieder", "David Puddy"];
+
+var question2 = ["Who is the NY Mets player that allegedly spit a loogie on Kramer and Newman?"];
+
+var answers2 = ["Gary Carter", "Darryl Strawberry", "Dwight Gooden", "Keith Hernandez"];
+
+function question(){
+    
+}
